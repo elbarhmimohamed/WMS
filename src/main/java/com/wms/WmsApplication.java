7a08d500fc572ad3e6164417f9745dfb52bb9673
hamzaelgannouni <adhamza2015@gmail.com>
2021-12-30 13:44:49 +0100

    add-run-in-main

--- src/main/java/com/wms/WmsApplication.java
+++ src/main/java/com/wms/WmsApplication.java
@@ -1,13 +1,43 @@
 package com.wms;
 
+
+import com.wms.model.personne.Users;
+import com.wms.repository.UsersRepository;
+import com.wms.services.UsersServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class WmsApplication {
+public class WmsApplication implements CommandLineRunner {
+
+	@Autowired
+	private UsersServices usersServices;
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(WmsApplication.class, args);
+
+
+
+
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Users admin = new Users();
+		admin.setName("hamza");
+		admin.setPassword("123456");
+		admin.setRole("Admin");
+		admin.setEmail("hamza@gmail.com");
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String encodedPassword = passwordEncoder.encode(admin.getPassword());
+		admin.setPassword(encodedPassword);
+
+		usersServices.saveUser(admin);
+	}
 }
